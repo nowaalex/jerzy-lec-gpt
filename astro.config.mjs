c3488757e71e0d9621e0ae5d84a14cbbbf63c2cc
alex <nowaalex@gmail.com>
2024-09-04 04:12:05 +0200

    added github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nowaalex.github.io",
+  base: "/jerzy-lec-gpt",
   integrations: [tailwind()],
 });
